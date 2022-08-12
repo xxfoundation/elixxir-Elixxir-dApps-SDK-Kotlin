c56621a0f5d00ded134c05a5699e3ee91ca68a87
Kamal Bramwell <kamal@elixxir.io>
2022-08-12 15:32:15 -0400

    Created CommonProperties interface

---
+++ app/src/main/java/io/elixxir/dapp/model/CommonProperties.kt
@@ -0,0 +1,6 @@
+package io.elixxir.dapp.model
+
+import io.elixxir.dapp.android.model.AndroidConfig
+import io.elixxir.dapp.logger.data.Logger
+
+internal interface CommonProperties : Logger, AndroidConfig
