0807a85ff0dd2e3abce449dbafdaa2098974d17d
Kamal Bramwell <kamal@elixxir.io>
2022-08-15 19:36:06 -0400

    Created UserDiscovery interface

--- app/src/main/java/io/elixxir/dapp/bindings/data/UserDiscovery.kt
+++ app/src/main/java/io/elixxir/dapp/bindings/data/UserDiscovery.kt
@@ -1,4 +1,16 @@
 package io.elixxir.dapp.bindings.data
 
 interface UserDiscovery {
+    fun registerUsername(username: String)
+    fun registerNickname(nickname: String)
+    fun registerEmail(email: String)
+    fun registerPhone(phone: String)
+    fun confirmTwoFactorAuth(tfaId: String, tfaCode: String)
+    fun removeEmail()
+    fun removePhone()
+    fun deleteUser()
+    fun findUserById(id: ByteArray)
+    fun usernameSearch(username: String)
+    fun phoneSearch(phone: String)
+    fun emailSearch(email: String)
 }
--- app/src/main/java/io/elixxir/dapp/bindings/data/UserDiscoveryAdapter.kt
+++ app/src/main/java/io/elixxir/dapp/bindings/data/UserDiscoveryAdapter.kt
@@ -3,4 +3,52 @@ package io.elixxir.dapp.bindings.data
 import bindings.UserDiscovery as CoreUserDiscovery
 
 @JvmInline
-value class UserDiscoveryAdapter(private val ud: CoreUserDiscovery) : UserDiscovery
+internal value class UserDiscoveryAdapter(private val ud: CoreUserDiscovery) : UserDiscovery {
+    override fun registerUsername(username: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerNickname(nickname: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerEmail(email: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerPhone(phone: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun confirmTwoFactorAuth(tfaId: String, tfaCode: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeEmail() {
+        TODO("Not yet implemented")
+    }
+
+    override fun removePhone() {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteUser() {
+        TODO("Not yet implemented")
+    }
+
+    override fun findUserById(id: ByteArray) {
+        TODO("Not yet implemented")
+    }
+
+    override fun usernameSearch(username: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun phoneSearch(phone: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun emailSearch(email: String) {
+        TODO("Not yet implemented")
+    }
+}
