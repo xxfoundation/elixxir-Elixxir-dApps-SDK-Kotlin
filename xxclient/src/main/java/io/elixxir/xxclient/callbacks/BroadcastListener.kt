cccd1d1d0fa27d03400c498de1228d1fecefaa9f
Kamal Bramwell <kamal@elixxir.io>
2022-08-24 14:47:19 -0400

    Created InvalidDataException, updated BroadcastListenerAdapter

--- xxclient/src/main/java/io/elixxir/xxclient/callbacks/BroadcastListener.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/callbacks/BroadcastListener.kt
@@ -4,19 +4,23 @@ import io.elixxir.xxclient.models.BindingsModel
 import java.lang.Exception
 import bindings.BroadcastListener as BroadcastListenerBindings
 import io.elixxir.xxclient.models.BroadcastMessage
+import io.elixxir.xxclient.models.InvalidDataException
 
 interface BroadcastListener {
     fun onBroadcast(result: Result<BroadcastMessage>)
 }
 
-open class BroadCastListenerAdapter(
+open class BroadcastListenerAdapter(
     protected val listener: BroadcastListener
 ) : BroadcastListener by listener, BroadcastListenerBindings {
+
     override fun callback(data: ByteArray?, error: Exception?) {
         val result: Result<BroadcastMessage> = error?.let {
             Result.failure(it)
         } ?: data?.let {
             Result.success(BindingsModel.decode(it, BroadcastMessage::class.java))
-        } ?: Result.failure(Throwable("Invalid data"))
+        } ?: Result.failure(InvalidDataException())
+
+        listener.onBroadcast(result)
     }
 }
---
+++ xxclient/src/main/java/io/elixxir/xxclient/models/InvalidDataException.kt
@@ -0,0 +1,3 @@
+package io.elixxir.xxclient.models
+
+class InvalidDataException(message: String = "Invalid data received") : Exception(message)
