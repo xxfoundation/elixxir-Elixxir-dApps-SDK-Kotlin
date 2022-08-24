462bd50920c5b09956d7507b64c0764eef662358
Kamal Bramwell <kamal@elixxir.io>
2022-08-24 13:19:11 -0400

    Created & implemented ClientErrorCallback interface

--- xxclient/src/main/java/io/elixxir/xxclient/callbacks/ClientErrorCallback.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/callbacks/ClientErrorCallback.kt
@@ -9,7 +9,7 @@ interface ClientErrorCallback {
 open class ClientErrorCallbackAdapter(
     protected val callback: ClientErrorCallback
 ) : ClientErrorCallback by callback, bindings.ClientError {
-    
+
     override fun report(source: String?, message: String?, trace: String?) {
         callback.handle(ClientError(source, message, trace))
     }
