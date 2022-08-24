f6d24ed65becc2118887eab9d4d897fab92b5fb5
Kamal Bramwell <kamal@elixxir.io>
2022-08-24 14:32:11 -0400

    Created & implemented MessageDeliveryListener interface

---
+++ xxclient/src/main/java/io/elixxir/xxclient/callbacks/MessageDeliveryListener.kt
@@ -0,0 +1,47 @@
+package io.elixxir.xxclient.callbacks
+
+import bindings.MessageDeliveryCallback
+import io.elixxir.xxclient.models.BindingsModel.Companion.decode
+import io.elixxir.xxclient.models.RoundResults
+
+interface MessageDeliveryListener {
+    fun onMessageSent(deliveryReport: Result<RoundResults>)
+}
+
+open class MessageDeliveryCallbackAdapter(
+    protected val listener: MessageDeliveryListener
+) : MessageDeliveryCallback {
+
+    override fun eventCallback(delivered: Boolean, timedOut: Boolean, roundResults: ByteArray?) {
+        when {
+            delivered -> roundResults?.let { onSuccess(it) }
+            timedOut -> onTimeOut()
+            else -> onFailed()
+        }
+    }
+
+    private fun onSuccess(roundResults: ByteArray) {
+        listener.onMessageSent(
+            Result.success(
+                decode(roundResults, RoundResults::class.java)
+            )
+        )
+    }
+
+    private fun onTimeOut() {
+        listener.onMessageSent(
+            Result.failure(DeliveryTimeOutException())
+        )
+    }
+
+    private fun onFailed() {
+        listener.onMessageSent(
+            Result.failure(DeliveryFailedException())
+        )
+    }
+}
+
+sealed class MessageDeliveryException : Exception()
+class DeliveryFailedException : MessageDeliveryException()
+class DeliveryTimeOutException : MessageDeliveryException()
+
---
+++ xxclient/src/main/java/io/elixxir/xxclient/models/RoundResults.kt
@@ -0,0 +1,5 @@
+package io.elixxir.xxclient.models
+
+data class RoundResults(
+    val roundIds: List<Long>
+) : BindingsModel
