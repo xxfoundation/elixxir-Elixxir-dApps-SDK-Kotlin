97b45e8fceac375676407f4dea97e91b4017ed41
Kamal Bramwell <kamal@elixxir.io>
2022-08-25 13:12:52 -0400

    Created & implemented UdSearchResultListener

--- xxclient/src/main/java/io/elixxir/xxclient/callbacks/UdSearchResultListener.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/callbacks/UdSearchResultListener.kt
@@ -1,17 +1,17 @@
 package io.elixxir.xxclient.callbacks
 
-import bindings.UdLookupCallback
+import bindings.UdSearchCallback
 import io.elixxir.xxclient.utils.ContactData
 import io.elixxir.xxclient.utils.parse
 import java.lang.Exception
 
-interface UdLookupResultListener {
-    fun onResponse(response: Result<ContactData>)
+interface UdSearchResultListener {
+    fun onResponse(result: Result<ContactData>)
 }
 
-open class UdLookupCallbackAdapter(
-    protected val listener: UdLookupResultListener
-) : UdLookupCallback {
+open class UdSearchCallbackAdapter(
+    protected val listener: UdSearchResultListener
+) : UdSearchCallback {
     override fun callback(contactData: ByteArray?, error: Exception?) {
         listener.onResponse(
             parse(contactData, error, ContactData::class.java)
