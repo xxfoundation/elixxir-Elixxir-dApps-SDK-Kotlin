fdb0571bfe118ae21c9ed7ad0ef64ccb55064e36
Kamal Bramwell <kamal@elixxir.io>
2022-10-18 09:25:17 -0400

    Changed registration ratio to float

--- xxclient/src/main/java/io/elixxir/xxclient/models/NodeRegistrationReport.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/models/NodeRegistrationReport.kt
@@ -4,6 +4,6 @@ data class NodeRegistrationReport(
     val registeredCount: Long,
     val totalCount: Long
 ) : BindingsModel {
-    val ratio: Long get() = registeredCount/totalCount
+    val ratio: Float get() = registeredCount.toFloat() / totalCount.toFloat()
 }
 
