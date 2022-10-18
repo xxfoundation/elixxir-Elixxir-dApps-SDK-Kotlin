a529fd4d4c6fccef208c9e2c2567fd1d0931daea
Kamal Bramwell <kamal@elixxir.io>
2022-10-18 14:36:49 -0400

    Removed PasswordStorage interface

--- xxclient/src/main/java/io/elixxir/xxclient/password/PasswordStorage.kt
+++
@@ -1,9 +0,0 @@
-package io.elixxir.xxclient.password
-
-import io.elixxir.xxclient.utils.Password
-
-interface PasswordStorage {
-    fun save(password: Password)
-    fun load(): Password
-    fun clear()
-}
