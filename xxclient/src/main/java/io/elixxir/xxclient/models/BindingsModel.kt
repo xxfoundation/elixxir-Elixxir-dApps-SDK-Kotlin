0b0daf5744e8cd9c775fe8a08e9cfbca9ac7ba6f
Kamal Bramwell <kamal@elixxir.io>
2022-10-21 11:29:04 -0400

    Updated serialization logic for encoding/decoding arrays

--- xxclient/src/main/java/io/elixxir/xxclient/models/BindingsModel.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/models/BindingsModel.kt
@@ -7,22 +7,22 @@ interface BindingsModel {
 
     companion object {
         fun encode(model: BindingsModel): ByteArray {
-            return Gson().toJson(model).toByteArray()
+            return Gson().toJson(model).encodeToByteArray()
         }
 
         inline fun <reified T> encodeArray(list: List<T>): ByteArray {
-            val typeToken = object : TypeToken<List<T>>() {}.type
-            return Gson().toJson(list, typeToken).toByteArray()
+            val typeToken = object : TypeToken<Array<T>>() {}.type
+            return Gson().toJson(list.toTypedArray(), typeToken).encodeToByteArray()
         }
 
         inline fun <reified T> decode(data: ByteArray): T {
             val typeToken = object : TypeToken<T>() {}.type
-            return Gson().fromJson(data.toString(), typeToken)
+            return Gson().fromJson(data.decodeToString(), typeToken)
         }
 
         inline fun <reified T> decodeArray(data: ByteArray): List<T> {
-            val typeToken = object : TypeToken<List<T>>() {}.type
-            return Gson().fromJson(data.toString(), typeToken)
+            val typeToken = object : TypeToken<Array<T>>() {}.type
+            return Gson().fromJson<Array<T>>(data.decodeToString(), typeToken).toList()
         }
     }
 }
