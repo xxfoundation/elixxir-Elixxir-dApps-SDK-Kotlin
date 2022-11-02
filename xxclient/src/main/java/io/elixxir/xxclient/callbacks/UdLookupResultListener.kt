42659258829f58ebc6d3655bc0722a9b1f765edd
Kamal Bramwell <kamal@elixxir.io>
2022-11-02 10:09:19 -0400

    Updated callback parameter from List<Contact> -> Contact

--- xxclient/src/main/java/io/elixxir/xxclient/callbacks/UdLookupResultListener.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/callbacks/UdLookupResultListener.kt
@@ -1,18 +1,20 @@
 package io.elixxir.xxclient.callbacks
 
+import android.util.Log
 import bindings.UdLookupCallback
 import io.elixxir.xxclient.utils.*
 import java.lang.Exception
 
 interface UdLookupResultListener {
-    fun onResponse(response: Result<List<ContactData>>)
+    fun onResponse(response: Result<ContactData>)
 }
 
 open class UdLookupCallbackAdapter(
     protected val listener: UdLookupResultListener
 ) : UdLookupCallback {
     override fun callback(contacts: ByteArray?, error: Exception?) {
-        val contactsResult = parseDataArray(
+        Log.d("UdLookup", "contacts: ${contacts?.decodeToString() ?: "null"}")
+        val contactsResult = parseData(
             contacts,
             error
         )
