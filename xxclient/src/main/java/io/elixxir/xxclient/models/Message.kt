4c0f99ba4931ecf10eb064755addd2972738b9ff
Kamal Bramwell <kamal@elixxir.io>
2022-11-09 10:27:32 -0500

    Updated Bindings Message model ByteArray args to Strings
    These strings must be decoded from base64 -> ByteArray by the client

--- xxclient/src/main/java/io/elixxir/xxclient/models/Message.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/models/Message.kt
@@ -1,16 +1,27 @@
 package io.elixxir.xxclient.models
 
+import com.google.gson.annotations.SerializedName
 import io.elixxir.xxclient.utils.Payload
 
 data class Message(
+    @SerializedName("MessageType")
     val messageType: Long,
-    val id: ByteArray,
-    val payload: Payload,
-    val sender: ByteArray,
-    val recipientId: ByteArray,
+    @SerializedName("ID")
+    val id: String,
+    @SerializedName("Payload")
+    val payload: String,
+    @SerializedName("Sender")
+    val sender: String,
+    @SerializedName("RecipientID")
+    val recipientId: String,
+    @SerializedName("EphemeralID")
     val ephemeralId: Long,
+    @SerializedName("Timestamp")
     val timestamp: Long,
+    @SerializedName("Encrypted")
     val encrypted: Boolean,
+    @SerializedName("RoundId")
     val roundId: Long,
+    @SerializedName("RoundURL")
     val roundUrl: String?
 ) : BindingsModel
