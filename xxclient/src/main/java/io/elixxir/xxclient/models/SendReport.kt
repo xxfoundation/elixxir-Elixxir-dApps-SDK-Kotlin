8ef20e7a70c70f1102fe88cfec0a59b8a9af2d15
Kamal Bramwell <kamal@elixxir.io>
2022-11-09 10:28:18 -0500

    Updated SendReport model ByteArray params -> String
    These Strings must be decoded from Base64 to ByteArray by the client

--- xxclient/src/main/java/io/elixxir/xxclient/models/SendReport.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/models/SendReport.kt
@@ -1,11 +1,17 @@
 package io.elixxir.xxclient.models
 
+import com.google.gson.annotations.SerializedName
 import io.elixxir.xxclient.utils.RoundId
 
 data class SendReport(
+    @SerializedName("Rounds")
     val roundIdList: List<RoundId>?,
-    val messageId: ByteArray?,
+    @SerializedName("MessageID")
+    val messageId: String?,
+    @SerializedName("Timestamp")
     val timestamp: Long?,
+    @SerializedName("RoundURL")
     val roundUrl: String?,
-    val keyResidue: ByteArray?
+    @SerializedName("KeyResidue")
+    val keyResidue: String?
 ) : BindingsModel
