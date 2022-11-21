4f503742f86e39a9806eb2c064e2c4e33c0cf73b
Kamal Bramwell <kamal@elixxir.io>
2022-11-21 10:33:05 -0500

    Assigned SerializedName for GroupReport fields

--- xxclient/src/main/java/io/elixxir/xxclient/models/GroupReport.kt
+++ xxclient/src/main/java/io/elixxir/xxclient/models/GroupReport.kt
@@ -1,8 +1,14 @@
 package io.elixxir.xxclient.models
 
+import com.google.gson.annotations.SerializedName
+
 data class GroupReport(
+    @SerializedName("Id")
     val id: String,
+    @SerializedName("Rounds")
     val rounds: List<Long>,
+    @SerializedName("RoundURL")
     val roundUrl: String,
+    @SerializedName("Status")
     val status: Long
 ) : BindingsModel
